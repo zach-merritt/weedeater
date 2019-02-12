15baee3037592b305b0cd1ccd905f85364c17bb3
John Blakkan <john.blakkan@berkeley.edu>
2019-02-11 22:56:39 -0800

    Changed image returned to a prettier one

--- sources/weedeater_map_demo_stub_image.R
+++ sources/weedeater_map_demo_stub_image.R
@@ -6,7 +6,7 @@ library(imager)
 
 df <- fromJSON("https://weedsample.herokuapp.com/return_sample_images/SPRAYER0002.json")
 
-image_url <- df$url[1] #there are two image URLs on SPRAYER0002, could pick either one of them.
+image_url <- df$url[2] #there are two image URLs on SPRAYER0002, could pick either one of them.
 
 jj <- load.image(image_url)
 plot(jj)
